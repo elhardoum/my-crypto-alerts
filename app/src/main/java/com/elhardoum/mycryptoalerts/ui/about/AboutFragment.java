31eafb632066a2d453f14d968d33e8ac33abf95c
elhardoum <elhardoum@outlook.com>
2022-02-24 14:02:00 -0700

    initial project with navigation and menus

---
+++ app/src/main/java/com/elhardoum/mycryptoalerts/ui/about/AboutFragment.java
@@ -0,0 +1,55 @@
+package com.elhardoum.mycryptoalerts.ui.about;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.elhardoum.mycryptoalerts.R;
+import com.elhardoum.mycryptoalerts.databinding.FragmentSlideshowBinding;
+
+public class AboutFragment extends Fragment {
+
+    private AboutViewModel viewModel;
+    private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+
+        viewModel =
+                new ViewModelProvider(this).get(AboutViewModel.class);
+
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textAbout;
+        viewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.add_item).setVisible(false);
+        super.onPrepareOptionsMenu(menu);
+    }
+}
